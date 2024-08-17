37e57956d8b5e78e57699f83bec26d7acbedb632
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-17 19:47:56 +0530

    Styling Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
-function ExpenseItem(){
-    return (
-        <div>
-        <p>15 August 2023</p>
-        <div>
-          <h2>Book</h2>
-          <h2>$10</h2>
-         <p>Delhi</p>
-        </div>       
+// Write your code at relevant places in the code below:
+
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div className="expense-item">15 August 2023</div>
+      <div className = "expense-item__description">
+        <h2 >Book</h2>
+        <div className="expense-item__price">$10</div>
+        <div className="expense-item__location">Delhi</div>
       </div>
-        )
+    </div>
+  );
 }
 
 export default ExpenseItem;
