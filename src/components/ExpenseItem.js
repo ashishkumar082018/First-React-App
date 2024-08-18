cabecce12fbd279bfc738a34708c643ad560947b
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-18 09:27:16 +0530

    Creating Reusable Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,7 +4,7 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
-      <div className = "expense-item__location">{props.location}</div>
+      <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
