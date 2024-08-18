0906241e6bfaf103a0841e8a9d3350d4c91eb5b3
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-18 09:39:48 +0530

    Wrapper Components

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,29 +1,18 @@
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  
-  const ExpenseDate = (props) => {
-    const month = months[props.date.getMonth()];
-    const year = props.date.getFullYear();
-    const date = props.date.getDate();
-    return (
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{date}</div>
-      </div>
-    );
-  };
-  
-  export default ExpenseDate; 
+import "./ExpenseDate.css";
+import Card from './Card'
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
+  return (
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </Card>
+  );
+}
+
+export default ExpenseDate;
