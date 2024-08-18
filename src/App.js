64e5ec521bded66ef532187a54d43ee62f578723
Ashish kumar <ashishkumar082018@gmail.com>
2024-08-18 09:23:44 +0530

    JavaScript Inside JSX

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-       <h1>Let's get Started</h1>
-       <ExpenseItem></ExpenseItem>
+      <h1>Let's get Started</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
-// Write your code at relevant places in the code below:
-
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore";
+
   return (
-    <div>
-      <div className="expense-item">15 August 2023</div>
-      <div className = "expense-item__description">
-        <h2 >Book</h2>
-        <div className="expense-item__price">$10</div>
-        <div className="expense-item__location">Delhi</div>
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{expenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
